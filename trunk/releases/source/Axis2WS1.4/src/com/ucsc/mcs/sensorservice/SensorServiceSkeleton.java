5d0059784f36cbcf9d217e276be1c06bec705a60
thisarattr@gmail.com <thisarattr@gmail.com@c899c864-b8be-2fb7-12bb-b1d18c64f838>
2011-09-27 20:38:35 +0000

    Sample mail send code added to loing operation to test it.

--- trunk/releases/source/Axis2WS1.4/src/com/ucsc/mcs/sensorservice/SensorServiceSkeleton.java
+++ trunk/releases/source/Axis2WS1.4/src/com/ucsc/mcs/sensorservice/SensorServiceSkeleton.java
@@ -15,6 +15,13 @@ import java.sql.Timestamp;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -124,6 +131,35 @@ public class SensorServiceSkeleton {
 		}
 		LoginResponseType res = new LoginResponseType();
 		res.setLoginResponseType(isLogin);
+		
+		//-------------------------------------------------------------------------------------------------------------
+		Session session = null;
+		try {
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+			session = (Session) envCtx.lookup("mail/JavaMail");
+		} catch (Exception ex) {
+			System.out.println("Error occurred while initiating mail context!!!");
+			System.out.println(ex.getMessage());
+		}
+		
+		Message message = new MimeMessage(session);
+		try {
+			message.setFrom(new InternetAddress("thisarattr@gmail.com"));
+			InternetAddress to[] = new InternetAddress[1];
+			to[0] = new InternetAddress("thisarattr@yahoo.com");
+			message.setRecipients(Message.RecipientType.TO, to);
+			message.setSubject("Test mail from my sensor web servivce!!!");
+			message.setContent("This is the message content", "text/html;charset=UTF-8");
+			Transport.send(message);
+		} catch (AddressException ex) {
+			System.out.println(ex.getMessage());
+		} catch (MessagingException e) {
+			System.out.println(e.getMessage());
+		}
+		
+ 
+ 		//-------------------------------------------------------------------------------------------------------------
 		return res;
 	}
 	
